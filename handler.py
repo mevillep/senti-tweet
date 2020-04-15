82f7c2e5c81bc3c953c9ae9b7a858a049cafeb46
meville <mevillepereira10@gmail.com>
2020-04-15 14:30:15 -0500

    add: query tweet

--- handler.py
+++ handler.py
@@ -1,13 +1,18 @@
 # handler.py
 
 import numpy as np
-
+from twitterscraper import query_tweets
 
 def main(event, context):
     #a = np.arange(15).reshape(3, 5)
 
     print("Access twitter data")
     #print(a)
+    list_of_tweets = query_tweets("Trump OR Clinton", 10)
+
+    #print the retrieved tweets to the screen:
+    for tweet in query_tweets("Trump OR Clinton", 10):
+        print(tweet)
 
 
 if __name__ == "__main__":
